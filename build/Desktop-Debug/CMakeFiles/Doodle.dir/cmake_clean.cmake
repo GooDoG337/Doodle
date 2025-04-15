35d77af71e2e760bb254ee9b8d57f3eccdf35de9
GooDoG337 <maxretro4rever@gmail.com>
2025-04-15 13:32:52 +0400

    Button's lifting?..

---
+++ build/Desktop-Debug/CMakeFiles/Doodle.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Doodle_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Doodle_autogen.dir/ParseCache.txt"
+  "Doodle_autogen"
+  "CMakeFiles/Doodle.dir/Doodle_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Doodle.dir/Doodle_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Doodle.dir/main.cpp.o"
+  "CMakeFiles/Doodle.dir/main.cpp.o.d"
+  "CMakeFiles/Doodle.dir/mainwindow.cpp.o"
+  "CMakeFiles/Doodle.dir/mainwindow.cpp.o.d"
+  "Doodle"
+  "Doodle.pdb"
+  "Doodle_autogen/mocs_compilation.cpp"
+  "Doodle_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Doodle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Desktop-Debug/CMakeFiles/Doodle_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Doodle_autogen"
+  "Doodle_autogen/include/ui_mainwindow.h"
+  "Doodle_autogen/mocs_compilation.cpp"
+  "Doodle_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Doodle_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
