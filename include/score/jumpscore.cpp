368c32f03c6e850cdd92399e64cd1802ff9f5a73
kelbiyev <kelbiyev11@mail.ru>
2025-04-28 22:01:03 +0400

    Changed score's font size. Why?. It didn't display the score >100000.

--- include/score/jumpscore.cpp
+++ include/score/jumpscore.cpp
@@ -3,7 +3,7 @@
 Score::Score(QWidget* parent): QWidget(parent){
     scoreLabel = new QLabel(this);
     scoreLabel->setText("Score: 0");
-    scoreLabel->setStyleSheet("font-size: 24px; color: white; background-color: rgba(0, 0, 0, 150); padding: 1px;");
+    scoreLabel->setStyleSheet("font-size: 16px; color: white; background-color: rgba(0, 0, 0, 150); padding: 1px;");
     scoreLabel->setGeometry(10, 10, 150, 40);  // позиция и размер на экране
     scoreLabel->show();
     connect(this, &Score::scoreChanged, this, &Score::updateScoreLabel);
